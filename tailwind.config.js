7d81cfe26c964e5d408aa2483e4a1cfbd7307769
Mohamad Lutfi Khoirudin <asiatakh25@gmail.com>
2024-08-22 20:51:00 +0700

    design: slicing header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,39 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: "75px",
+    },
+    extend: {
+      fontFamily: {
+        sans: ['"Poppins"', ...defaultTheme.fontFamily.sans],
+        inter: ['"Inter"'],
+      },
+      spacing: {
+        7.5: "30px",
+        12.5: "50px",
+        15: "60px",
+      },
+      colors: {
+        brand: {
+          primary: "#B88DFF",
+          dark: "#080318",
+        },
+      },
+      animation: {
+        "slider": "scroll 60s linear infinite",
+      },
+      keyframes: {
+        scroll: {
+          "0%": { transform: "translateX(0)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
